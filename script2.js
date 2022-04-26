6e769a30ce0902c78d3d98d5ee18358ce93455cc
Yury <kazantsevym@mail.ru>
2022-04-26 16:21:37 +0300

    new code with cikl

--- script2.js
+++ script2.js
@@ -8,12 +8,32 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB)
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+            
+            if ( a != null && b != null && a !='' && b!='' && a.length <50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('Error');
+                i--;
+            }
+}
+
+
+
+if(personalMovieDB.count < 10) {
+    console.log("Мало");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Classic');
+} else if (personalMovieDB.count >= 30) {
+    console.log('kinoman');
+} else {
+    console.log('oshibka');
+
+}
+
+console.log(personalMovieDB);
